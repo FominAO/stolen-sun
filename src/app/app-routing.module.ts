80c47f58073007b0108027d3327fc6877282b3a2
FominAO <fomagoafterme@yandex.ru>
2021-06-22 16:34:52 +0300

    add dist

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo:'main-menu' },
+  { path: 'main-menu', loadChildren: () => import('./main-menu/main-menu.module').then(m => m.MainMenuModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/main-menu/main-menu.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-menu',
+  templateUrl: './main-menu.component.html',
+  styleUrls: ['./main-menu.component.scss']
+})
+export class MainMenuComponent implements OnInit {
+  menuItems: {label: string, path: string}[] = [
+    {
+      label: 'Continue',
+      path: 'continue'
+    },
+    {
+      label: 'New Game',
+      path: 'new'
+    },
+    {
+      label: 'Settings',
+      path: 'settings'
+    },
+    {
+      label: 'Achievements',
+      path: 'achievements'
+    },
+    {
+      label: 'Exit Game',
+      path: 'loading'
+    },
+  ] 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
